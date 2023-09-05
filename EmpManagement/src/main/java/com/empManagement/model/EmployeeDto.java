c10d7354666b403029d1e71e553e18e920d2a81e
bhanu <bhanu@mail.com>
2023-09-05 17:37:20 +0530

    Create Employee Dto

---
+++ EmpManagement/src/main/java/com/empManagement/model/EmployeeDto.java
@@ -0,0 +1,36 @@
+package com.empManagement.model;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Data;
+
+@Data
+@Component
+@JsonInclude(value = Include.NON_NULL)
+ public class EmployeeDto {
+ 
+	private long id;
+ 
+	private String firstName;
+ 
+	private String lastName;
+ 
+	private String emailId;
+	 
+	private String password;
+	 	
+	private String userName;
+	
+	private String image;
+	 
+	private String dateCreated;
+ 
+	private String dateModified;
+ 
+	private String createdBy;
+	
+	private String modifiedBy;
+}
